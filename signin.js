5ea0da55597b1e28c51e5f92b07e1e3edb082bd5
san-jeev <55067454+san-jeev@users.noreply.github.com>
2019-10-02 16:26:31 +0530

    Add files via upload

--- signin.js
+++ signin.js
@@ -8,7 +8,7 @@ var password = document.getElementById("password").value;
 if (username == "facerek" && password == "1234"){
 	document.location.assign('https://project5.onrender.com/image-classify.html');
 	alert ("Login successfully");
-	return true;
+	return false;
 }
 else{
 	alert("You have entered Invalid Username or Pasword; Please Retry");
