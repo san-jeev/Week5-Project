bff0372bf4cb47debcc580b85942a07171ef1d4f
san-jeev <55067454+san-jeev@users.noreply.github.com>
2019-10-02 17:08:49 +0530

    Add files via upload

--- signin.js
+++ signin.js
@@ -7,8 +7,13 @@ var password = document.getElementById("password").value;
 
 if (username == "facerek" && password == "1234"){
 	document.location.assign('https://project5.onrender.com/image-classify.html');
+        alert("location.assign done");	
+	document.location.replace('https://project5.onrender.com/image-classify.html');
+        alert("location.replace done");
+	document.location.href('https://project5.onrender.com/image-classify.html');
+        alert("location.href done");
 	alert ("Login successfully");
-        alert("In Validate signin.js");
+        alert("In Validate signin.js now going returning");
 	return false;
 }
 else{
